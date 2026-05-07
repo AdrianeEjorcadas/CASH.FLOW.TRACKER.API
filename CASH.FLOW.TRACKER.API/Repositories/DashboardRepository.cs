d70d84b69694315bdc6eaf84e23e569550219c8a
Adriane E. <adrianeejorcadas97@gmail.com>
2026-05-07 16:50:28 +0800

    05/07/26 - add initial dashbaord data

---
+++ CASH.FLOW.TRACKER.API/Repositories/DashboardRepository.cs
@@ -0,0 +1,40 @@
+﻿using BUGET.TRACKER.API.Data;
+using BUGET.TRACKER.API.Model;
+using CASH.FLOW.TRACKER.API.Model.DTO.Dashboard;
+using CASH.FLOW.TRACKER.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CASH.FLOW.TRACKER.API.Repositories
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<InitialDataDto>> GetInitialData(GetInitialDataDto dto, CancellationToken ct)
+        {
+            var currentMonth = DateTime.Now.Month;
+            var currentYear = DateTime.Now.Year;
+
+            var payload = await _context.Transactions
+                .AsNoTracking()
+                .Include(t => t.Category)
+                .Where(t => t.UserId == dto.userId
+                    && t.TransactionDate >= dto.startDate
+                    && t.TransactionDate <= dto.endDate)
+                .GroupBy(t => t.Category.CategoryType)
+                .Select(g => new InitialDataDto 
+                {
+                    CategoryType = g.Key,
+                    Amount = g.Sum(x => x.Amount)
+                })
+                .ToListAsync(ct);
+
+            return payload;
+        }
+    }
+}
---
+++ dbo.Transactions.data.sql
@@ -0,0 +1,20 @@
+﻿SELECT b.CategoryType, sum(a.Amount) as "total amount" FROM Transactions a
+join Categories b
+on a.CategoryId = b.CategoryId
+where a.UserId = 'ced9f3da-ca69-4b1a-52bd-08de9b76e356'
+and a.TransactionDate between '2026-04-01' and '2026-04-30'
+group by b.CategoryType;
+
+
+
+
+SELECT a.CategoryId, a.CategoryName, a.CategoryType from Categories a;
+
+
+
+
+
+where a.UserId = 'ced9f3da-ca69-4b1a-52bd-08de9b76e356'
+and a.TransactionDate between '2026-05-01' and '2026-05-30'
+group by b.CategoryType
+order by b.CategoryType
